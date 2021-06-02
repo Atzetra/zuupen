d74156a09ee34200faf3a5b77c6e0e25365f912a
Enzo Sastrokarijo <e.sastrokarijo@gmail.com>
2021-06-02 04:06:27 +0200

    Init App

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Zuupen',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Material App Bar'),
