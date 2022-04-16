44a20f32d08186b124b715da7f3591a459bede6d
Enzo Sastrokarijo <enzo@sastrokarijo.nl>
2022-04-16 02:41:38 +0200

    Make playerlist scrollable

--- lib/views/game_screen.dart
+++ lib/views/game_screen.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 
 import '../controllers/cards_controller.dart';
 import '../controllers/game_controller.dart';
